97463f8787d4ea66a33f6cb91c5b1916bcd2aad3
imtiajul-islam-d <imtiajul@hotmail.com>
2023-04-02 19:11:50 +0600

    take % as input along with number

--- 2_module-1/output.c
+++ 2_module-1/output.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    int emi;
-    int numayeer;
-    int ishat;
-    char a;
-    float f;
-    scanf("%d %d %d %c %f", &emi, &numayeer, &ishat, &a, &f);
-    printf("%d %d %d %c %0.1f", emi, numayeer, ishat, a, f);
+    // int emi;
+    // int numayeer;
+    // int ishat;
+    // char a;
+    // float f;
+    // scanf("%d %d %d %c %f", &emi, &numayeer, &ishat, &a, &f);
+    // printf("%d %d %d %c %0.1f", emi, numayeer, ishat, a, f);
     // scanf("%c", &a);
     // printf("%c", a);
+
+    // int a, b;
+    // char p;
+    // scanf("%d%c %d%c", &a, &p, &b, &p);
+    // printf("%d%% %d%%", a, b);
+    int a, b;
+    scanf("%d%% %d%%", &a, &b);
+    printf("%d%% %d%%", a, b);
     return 0;
 }
