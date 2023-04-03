0fa93f7385242de91cda94e55cdfb292b237b195
imtiajul-islam-d <imtiajul@hotmail.com>
2023-04-03 20:59:59 +0600

    nested if else added

---
+++ 3_module-2/nested_condition.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if (tk > 12000)
+    {
+        printf("Cox's bazar colo\n");
+        if (10000)
+        {
+            printf("Saint martin\n");
+        }
+        else
+        {
+            printf("Ferot cole jao\n");
+        }
+    }
+    else if (tk > 5000)
+    {
+        printf("Tong a jao");
+    }
+    else
+    {
+        printf("Kothao jabo na!!");
+    }
+
+    return 0;
+}
