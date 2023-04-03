b4b1f2edd47e30dadec8dbbd6305df49bde7a484
imtiajul-islam-d <imtiajul@hotmail.com>
2023-04-03 19:56:50 +0600

    arithmetic operators

---
+++ 3_module-2/arithmetic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 23;
+    // int sum = a + b;
+    // printf("%d", sum);
+    // int minus = a - b;
+    // printf("%d", minus);
+    // int multiply = a * b;
+    // printf("%d", multiply);
+    // int division = b / a;
+    // printf("%d", division);
+    // int module = b % a;
+    // printf("%d", module);
+    // float a = 10, b = 22;
+    // float multiply = b / a;
+    // printf("%f", multiply);
+    return 0;
+}
