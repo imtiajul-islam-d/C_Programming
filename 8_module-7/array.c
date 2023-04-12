aa48ea563e3d9a14ef47525ff9c456035d418b5f
imtiajul-islam-d <imtiajul@hotmail.com>
2023-04-12 11:39:37 +0600

    sum of an array

--- 8_module-7/array.c
+++ 8_module-7/array.c
@@ -28,14 +28,16 @@ int main()
     int n;
     scanf("%d", &n);
     int arr[n];
+    int sum = 0;
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
     for (int i = n - 1; i >= 0; i--)
     {
-        printf("%d\n", arr[i]);
+        sum += arr[i];
     }
+    printf("%d", sum);
 
     return 0;
 }
