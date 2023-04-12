cf81a8fde933d736f08173d031856b3ee52c0510
imtiajul-islam-d <imtiajul@hotmail.com>
2023-04-12 11:24:30 +0600

    array practice

---
+++ 8_module-7/array.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int array[10];
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         scanf("%d", &array[i]);
+//         printf("%d\n", array[i]);
+//     }
+
+//     return 0;
+// }
+#include <stdio.h>
+int main()
+{
+    int arr[] = {3, 4, 55, 5};
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
