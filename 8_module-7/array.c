99bfee402888abd44ce579d7965c201fca254f67
imtiajul-islam-d <imtiajul@hotmail.com>
2023-04-12 11:37:12 +0600

    array loop added

--- 8_module-7/array.c
+++ 8_module-7/array.c
@@ -11,11 +11,28 @@
 
 //     return 0;
 // }
+// #include <stdio.h>
+// int main()
+// {
+//     int arr[] = {3, 4, 55, 5};
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%d\n", arr[i]);
+//     }
+
+//     return 0;
+// }
 #include <stdio.h>
 int main()
 {
-    int arr[] = {3, 4, 55, 5};
-    for (int i = 0; i < 4; i++)
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = n - 1; i >= 0; i--)
     {
         printf("%d\n", arr[i]);
     }
