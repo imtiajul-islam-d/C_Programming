e11776cb7d6be81e6073776cc7a2cd44177856bc
imtiajul-islam-d <imtiajul@hotmail.com>
2023-04-30 16:07:11 +0600

    pattern 4

--- 14_module-13/tempCodeRunnerFile.c
+++ 14_module-13/tempCodeRunnerFile.c
@@ -1,4 +1,16 @@
-for (int s = star; s <= n; s++)
-    // {
-    //     printf("%c", '*');
-    // }
+or (int j = 1; j <= s; j++)
+    {
+        printf(" ");
+    }
+    for (int j = 1; j <= k; j++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    if(i <= n - 1){
+        n --;
+        k += 2;
+    }else{
+        n ++;
+        k -=2;
+    }
