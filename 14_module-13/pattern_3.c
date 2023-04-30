21ef70972cf517435f2eca6a8a67372687866cf9
imtiajul-islam-d <imtiajul@hotmail.com>
2023-04-30 08:33:06 +0600

    pattern 3 reverse

--- 14_module-13/pattern_3.c
+++ 14_module-13/pattern_3.c
@@ -1,16 +1,33 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int number = 1;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= number; j++)
+//         {
+//             printf("d");
+//         }
+//         number++;
+//         printf("\n");
+//     }
+//     return 0;
+// }
 #include <stdio.h>
 int main()
 {
     int n;
     scanf("%d", &n);
-    int number = 1;
-    for (int i = 1; i <= n; i++)
+    int number = n;
+    for (int i = n; i >= 1; i--)
     {
         for (int j = 1; j <= number; j++)
         {
             printf("d");
         }
-        number++;
+        number--;
         printf("\n");
     }
     return 0;
