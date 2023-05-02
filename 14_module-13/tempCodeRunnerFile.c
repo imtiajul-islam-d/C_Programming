6359fb85fc448e4804592b9f4e69d5bcdaa18e79
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-02 15:39:42 +0600

    array sort

--- 14_module-13/tempCodeRunnerFile.c
+++ 14_module-13/tempCodeRunnerFile.c
@@ -1,16 +1,4 @@
-or (int j = 1; j <= s; j++)
-    {
-        printf(" ");
-    }
-    for (int j = 1; j <= k; j++)
-    {
-        printf("*");
-    }
-    printf("\n");
-    if(i <= n - 1){
-        n --;
-        k += 2;
-    }else{
-        n ++;
-        k -=2;
-    }
+i < n; i++)
+      {
+        printf("%d\n", arr[i]);
+      }
