04d43f67732978571d7abe261eab835f3f1a940a
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-03 06:25:05 +0600

    function

--- 15_module-14/function.c
+++ 15_module-14/function.c
@@ -1,12 +1,46 @@
+// #include <stdio.h>
+// int sum (int x, int y)
+// {
+//     int sum = x + y;
+//     return sum;
+// }
+// int main()
+// {
+//   int s = sum(10, 20);
+//   printf("%d", s);
+//   return 0;
+// }
+// #include <stdio.h>
+// void sum (int x, int y)
+// {
+//     int sum = x + y;
+//     printf("%d", sum);
+// }
+// int main()
+// {
+//   sum(103, 20);
+//   return 0;
+// }
+// #include <stdio.h>
+// void sum (void)
+// {
+//     int sum = 10 + 20;
+//     printf("%d", sum);
+// }
+// int main()
+// {
+//   sum();
+//   return 0;
+// }
 #include <stdio.h>
-int sum (int x, int y)
+int sum (void)
 {
-    int sum = x + y;
+    int sum = 20 + 20;
     return sum;
 }
 int main()
 {
-  int s = sum(10, 20);
+  int s = sum();
   printf("%d", s);
   return 0;
 }
