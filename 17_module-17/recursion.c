e31d2cacaaa112b0b7b9fdae00ac6954a2128bdb
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-10 08:45:56 +0600

    recursion

--- 17_module-17/recursion.c
+++ 17_module-17/recursion.c
@@ -26,24 +26,47 @@
 //     num(5);
 //     return 0;
 // }
+// #include <stdio.h>
+// void num(int arr[], int n, int i)
+// {
+//     if (i == n)
+//         return;
+//     printf("%d\n", arr[i]);
+//     num(arr, n, i + 1);
+// }
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d ", &arr[i]);
+//         // printf("%d ", arr[i]);
+//     }
+//     num(arr, n, 0);
+//     return 0;
+// }
+
 #include <stdio.h>
-void num(int arr[], int n, int i)
+void num(int i, char parameter)
 {
-    if (i == n)
+    if (i == 6)
         return;
-    printf("%d\n", arr[i]);
-    num(arr, n, i + 1);
+    if (parameter == 'r')
+    {
+        num(i + 1, parameter);
+        printf("%d\n", i);
+    }
+    if (parameter == 'n')
+    {
+        printf("%d\n", i);
+        num(i + 1, parameter);
+    }
 }
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d ", &arr[i]);
-        // printf("%d ", arr[i]);
-    }
-    num(arr, n, 0);
+    // printf("Printing\n");
+    num(1, 'n');
     return 0;
 }
