2b556a6f1f314acac010e8dfe64afcef0e5ab5a7
imtiajul-islam-d <imtiajul@hotmail.com>
2023-05-10 07:59:03 +0600

    array

--- 17_module-17/recursion.c
+++ 17_module-17/recursion.c
@@ -1,14 +1,49 @@
+// #include <stdio.h>
+// void num(int i)
+// {
+//     if (i == 6)
+//         return;
+//     printf("%d\n", i);
+//     num(i + 1);
+// }
+// int main()
+// {
+//     printf("Printing\n");
+//     num(1);
+//     return 0;
+// }
+// #include <stdio.h>
+// void num(int i)
+// {
+//     if (i == 0)
+//         return;
+//     printf("%d\n", i);
+//     num(i - 1);
+// }
+// int main()
+// {
+//     printf("Printing\n");
+//     num(5);
+//     return 0;
+// }
 #include <stdio.h>
-void num(int i)
+void num(int arr[], int n, int i)
 {
-    if (i == 6)
+    if (i == n)
         return;
-    printf("%d\n", i);
-    num(i + 1);
+    printf("%d\n", arr[i]);
+    num(arr, n, i + 1);
 }
 int main()
 {
-    printf("Printing\n");
-    num(1);
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d ", &arr[i]);
+        // printf("%d ", arr[i]);
+    }
+    num(arr, n, 0);
     return 0;
 }
